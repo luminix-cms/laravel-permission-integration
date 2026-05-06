190ae3b076f78736e3e0bdb0fc874e1fa97caab7
Bruno Polo <brunopolo@poli.ufrj.br>
2026-05-06 12:34:06 -0300

    chore: add frontend package and update service provider
    - Added `luminix/frontend` to `composer.json`.
    - Updated `src/PermissionServiceProvider.php` to include new services and configurations.

--- src/PermissionServiceProvider.php
+++ src/PermissionServiceProvider.php
@@ -5,6 +5,8 @@ namespace Luminix\LaravelPermissionIntegration;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Luminix\Backend\Services\ModelFinder;
+use Luminix\Frontend\Services\BootService;
+use Luminix\LaravelPermissionIntegration\Facades\Integration;
 use Luminix\LaravelPermissionIntegration\Models\Permission;
 use Luminix\LaravelPermissionIntegration\Models\Role;
 use Luminix\LaravelPermissionIntegration\Services\IntegrationService;
@@ -14,19 +16,21 @@ class PermissionServiceProvider extends ServiceProvider
 
     public function register()
     {
-
+        $this->bindPackageServices();
         $this->makeLuminixFindModels();
-
         $this->loadTranslations();
-
-        $this->app->bind(IntegrationService::class, function () {
-            return new IntegrationService();
-        });
-
     }
 
     public function boot()
     {
+        $this->addFrontendConfigurations();
+    }
+
+    protected function bindPackageServices()
+    {
+        $this->app->bind(IntegrationService::class, function () {
+            return new IntegrationService();
+        });
 
     }
 
@@ -62,5 +66,16 @@ class PermissionServiceProvider extends ServiceProvider
 
         $this->loadJsonTranslationsFrom(__DIR__ . '/../lang');
 
+    }
+
+    protected function addFrontendConfigurations()
+    {
+        BootService::reducer('wireConfig', fn ($config) => [
+            ...$config,
+            'permission' => [
+                ...($config['permission'] ?? []),
+                'available_guards' => Integration::getAvailableGuards(),
+            ]
+        ]);
     }
 }
